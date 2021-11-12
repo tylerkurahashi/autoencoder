f63339cdfbb6d4f07affc648937e0724147bb9b3
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-11-12 17:10:06 +0900

    init

---
+++ net/__init__.py
@@ -0,0 +1,3 @@
+from .AutoEncoder import AutoEncoder, ConvAutoEncoder
+from .Classifier import Classifier, ConvClassifier
+__all__ = ('AutoEncoder', 'ConvClassifier', 'Classifier', 'ConvAutoEncoder')
